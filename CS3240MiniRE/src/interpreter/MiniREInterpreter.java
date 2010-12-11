b02d7f78a9d8f676d4b543f8ee4fdd9a2b8b9f7b
aaron <goldsmithaaron@gmail.com>
2010-12-11 14:35:15 -0500

    Have implemented everything except actually executing the tokens. So can now verify that the program begins and ends on the appropriate terminals.

--- CS3240MiniRE/src/interpreter/MiniREInterpreter.java
+++ CS3240MiniRE/src/interpreter/MiniREInterpreter.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 public class MiniREInterpreter {
 
 	public void interpretTokens(List<MiniREToken> tokencollection) {
+		boolean ended = false;
+		boolean started = false;
 		if(tokencollection == null || tokencollection.size() == 0) {
 			System.out.println("Token collection passed to interpretTokens() was not usable because:");
 			if(tokencollection == null) {
@@ -30,10 +32,44 @@ public class MiniREInterpreter {
 		}
 		List<MiniREToken> buff = new ArrayList<MiniREToken>();
 		for(MiniREToken tok : tokencollection) {
+			if(ended) {
+				//Then we have additional tokens when we shouldn't have.
+				System.out.println("The program continues after it supposedly ends.");
+				System.out.println("See line " + tok.getLinenum() + " in the program.");
+				System.exit(0);
+			}
+			
+			buff.add(tok);
 			if(tok.getTokentype() == MiniREToken.Type.TERMINAL) {
 				//Is it the the semicolon terminal.
 				if(tok.getTerm() == Terminal.semicolon) {
-					
+					executeTokens(buff);
+					buff.clear();
+				}
+				else if(tok.getTerm() == Terminal.begin) {
+					if(buff.size() > 1) {
+						//For some reason we are getting a begin terminal when it isn't the
+						//start of the program.
+						System.out.println("The 'begin' terminal is not expected on line " + tok.getLinenum());
+						System.exit(0);
+					}
+					//Let us know that the program has been started.
+					started = true;
+					buff.clear();
+				}
+				else if(tok.getTerm() == Terminal.end) {
+					if(buff.size() > 1) {
+						//Then there is more than just "end" in the buffer.
+						//We won't get here if the previous token is a ';'.
+						//Therefore, the previous "line" of the program is 
+						//invalid and we can go ahead and exit out of it.
+						System.out.println("The 'end' terminal was hit without the previous line ending in a ';'");
+						System.out.println("The 'end' terminal is found on line " + tok.getLinenum());
+						System.exit(0);
+					}
+					//Set the boolean that indicates that we should be done.
+					ended = true;
+					buff.clear();
 				}
 			}
 			else if(tok.getTokentype() == MiniREToken.Type.LEXICAL) {
@@ -45,10 +81,16 @@ public class MiniREInterpreter {
 				System.out.println("At line " + tok.getLinenum() + " is invalid.");
 				System.exit(0);
 			}
+			
+			if(!started) {
+				//The first token in the program should start us properly.
+				System.out.println("The first terminal in the program was not 'begin'.");
+				System.exit(0);
+			}
 		}
 	}
 	
-	public void executeTokens(MiniREToken tok) {
+	public void executeTokens(List<MiniREToken> tokens) {
 		
 	}
 }
