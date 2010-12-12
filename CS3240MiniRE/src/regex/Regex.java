41cfa0350c65e173791e6ef0e9cb9f86a8ea038c
Lily <lily@voidlily.net>
2010-12-12 02:35:19 -0500

    Regex stub

---
+++ CS3240MiniRE/src/regex/Regex.java
@@ -0,0 +1,21 @@
+package regex;
+
+import java.util.List;
+
+public class Regex {
+	private String str;
+	private boolean compiled = false;
+
+	public Regex(final String str) {
+		this.str = str;
+	}
+
+	public List<Match> match(final String buf) {
+		// TODO stub
+		return null;
+	}
+
+	public void replace(final String buf, final String rep) {
+		// TODO stub
+	}
+}
