5cc72ace09d43b339cb9514aaa0e9c225be10e52
Lily <lily@voidlily.net>
2010-12-12 21:49:28 -0500

    if/while blocks (bool_op not implemented, compile errors)

--- CS3240MiniRE/src/parser/MiniREParser.java
+++ CS3240MiniRE/src/parser/MiniREParser.java
@@ -12,23 +12,20 @@
 
 package parser;
 
-import grammar.GrammarRule;
 import grammar.Lexical;
 import grammar.Terminal;
+import interpreter.MiniREInterpreter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
-import interpreter.MiniREInterpreter;
-import parser.MiniREToken.Type;
-import regex.Match;
+import java.util.ListIterator;
 
 import minire.CharacterHelper;
+import regex.Match;
 
 /**
  * @author Incomprehensible Penguin Arena
@@ -37,13 +34,14 @@ public class MiniREParser {
 	private String programfilepath;
 	private FileReader programfilereader;
 	private MiniREToken[] tokencollection;
+	private List<MiniREToken> toks;
 	private MiniREScanner scanner;
 
 	public MiniREParser() throws IOException {
 		scanner = new MiniREScanner(programfilereader);
-		
+
 	}
-	
+
 	private MiniREInterpreter interp = new MiniREInterpreter();
 
 	/**
@@ -63,11 +61,11 @@ public class MiniREParser {
 			}
 		}
 		else {
-			
+
 			//Then the inputed file does not exist, raise an error accordingly.
 		}
 	}
-	
+
 	/**
 	 * Creates the tokens that represent the file and checks each against the
 	 * grammar.
@@ -78,7 +76,7 @@ public class MiniREParser {
 		List<MiniREToken> tokens = scanner.scan();
 		return tokens;
 	}
-	
+
 	public List<List<MiniREToken>> getTokensByLines(List<MiniREToken> tokens) throws IOException {
 		List<List<MiniREToken>> ret = new ArrayList<List<MiniREToken>>();
 		List<MiniREToken> current_line = new ArrayList<MiniREToken>();
@@ -87,7 +85,7 @@ public class MiniREParser {
 			if(Terminal.determineTokenType(t.getTokenstr())==Terminal.semicolon) {
 				t.setLinenum(t.getLinenum()-1);
 			}
-			
+
 			if(t.getLinenum() > linenum) {
 				ret.add(current_line);
 				current_line = new ArrayList<MiniREToken>();
@@ -100,11 +98,11 @@ public class MiniREParser {
 		ret.add(current_line); //adds final line
 		return ret;
 	}
-	
-	Iterator<MiniREToken> itr;
-	
+
+	ListIterator<MiniREToken> itr;
+
 	MiniREToken token;
-	
+
 	private boolean match(String ExpectedName) {
 		MiniREToken ExpectedToken = new MiniREToken(ExpectedName);
 		if (token.getTokenstr().equals(ExpectedToken.getTokenstr()) && token.getTokentype().equals(ExpectedToken.getTokentype())){
@@ -117,7 +115,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	private boolean match(Terminal term) {
 		if (token.getTokentype()==MiniREToken.Type.TERMINAL && token.getTerm() == term) {
 			token = itr.next();
@@ -127,7 +125,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	private boolean match(Lexical lex) {
 		if (token.getTokentype()==MiniREToken.Type.LEXICAL && token.getLex() == lex) {
 			token = itr.next();
@@ -137,7 +135,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	/** matches current token without iterating forward */
 	private boolean cmatch(String ExpectedName) {
 		MiniREToken ExpectedToken = new MiniREToken(ExpectedName);
@@ -148,7 +146,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	/** matches current token without iterating forward */
 	private boolean cmatch(Terminal term) {
 		if (token.getTokentype()==MiniREToken.Type.TERMINAL && token.getTerm() == term) {
@@ -158,7 +156,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	/** matches current token without iterating forward */
 	private boolean cmatch(Lexical lex) {
 		if (token.getTokentype()==MiniREToken.Type.LEXICAL && token.getLex() == lex) {
@@ -168,7 +166,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	/** matches next token without iterating forward */
 	private boolean nmatch(String ExpectedName) {
 		MiniREToken ExpectedToken = new MiniREToken(ExpectedName);
@@ -179,7 +177,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	/** matches next token without iterating forward */
 	private boolean nmatch(Terminal term) {
 		if (itr.next().getTokentype()==MiniREToken.Type.TERMINAL && itr.next().getTerm() == term) {
@@ -189,7 +187,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	/** matches next token without iterating forward */
 	private boolean nmatch(Lexical lex) {
 		if (itr.next().getTokentype()==MiniREToken.Type.LEXICAL && itr.next().getLex() == lex) {
@@ -199,7 +197,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	private void begin() throws IOException {
 		//begin <statement-list> end
 		match("begin");
@@ -209,11 +207,11 @@ public class MiniREParser {
 			statement();
 			token = itr.next();
 		}
-		if (token==null) {       
+		if (token==null) {
 			System.err.println("EOF: no matching end terminal");
-        }
+		}
 	}
-	
+
 	private void statement() throws IOException {
 		//<statement> <statement-list>
 		//System.out.println(token);
@@ -223,9 +221,98 @@ public class MiniREParser {
 				print();
 		} else if (match(Lexical.ID)) {
 				id();
+		} else if (match("if")) {
+				ifblock();
 		}
 	}
-	
+
+	private void ifblock() {
+		match("if");
+		token = itr.next();
+		match("(");
+		token = itr.next();
+		boolean cond = bool_op(); // TODO incomplete
+		token = itr.next();
+		match(")");
+		token = itr.next();
+		match("{");
+		while (!nmatch("}")) {
+			token = itr.next();
+			if (cond) {
+				statement();
+			}
+		}
+		token = itr.next();
+		if (match("else")) {
+			token = itr.next();
+			match("{");
+			while (!nmatch("}")) {
+				token = itr.next();
+				if (!cond) {
+					statement();
+				}
+			}
+			token = itr.next();
+		}
+	}
+
+	private void whileblock() {
+		ListIterator<MiniREToken> orig = itr;
+		match("while");
+		token = itr.next();
+		nmatch("("); // nmatch to get the correct cond_index
+
+		int cond_index = itr.nextIndex();
+		ListIterator<MiniREToken> condItr = toks.listIterator(cond_index);
+
+		token = itr.next();
+		boolean cond = bool_op();
+		match(")");
+
+		token = itr.next();
+		nmatch("{"); // nmatch to get the correct block_index
+		int block_index = itr.nextIndex();
+		token = itr.next();
+
+		while (true) {
+			int nested = 1;
+			// Repeat the following block for two cases:
+			// 1. The condition is true - we will recursively execute all nested blocks,
+			// so it is sufficient to match RCURLY
+			// 2. The condition is false - we need to track nested blocks to make sure match
+			// the correct RCURLY, as none of the nested blocks will be executed
+			while (!nmatch("}") || nested > 0) {
+				token = itr.next();
+				if (cond) {
+					statement();
+				} else {
+					// if the condition is false, keep count of any nested blocks,
+					// as we're skipping over but not executing them
+					if (nmatch("{")) {
+						nested++;
+					} else if (nmatch("}")) {
+						nested--;
+					}
+				}
+			}
+			// check the condition
+			orig = itr;
+			itr = condItr;
+			cond = bool_op(); // TODO incomplete
+			// reset the condition iterator
+			condItr = toks.listIterator(cond_index);
+			if (cond) {
+				// if condition holds, reset the iterator to the start of the block
+				itr = toks.listIterator(block_index);
+			} else {
+				// The condition is false, so after resetting the iterator,
+				// break out of the infinite loop
+				itr = orig;
+				break;
+			}
+		}
+	}
+
 	private void replace() throws IOException {
 		System.out.println("check replace");
 		//replace REGEX with ASCIISTR in <file-names> ;
@@ -250,7 +337,7 @@ public class MiniREParser {
 		System.out.println(token);
 		//interp.replace(regex, repl, filenames[0], filenames[1]);
 	}
-	
+
 	private void print() throws IOException {
 		//print ( <exp-list> ) ;
 		match("print");
@@ -258,13 +345,13 @@ public class MiniREParser {
 		exp_list();
 		match(")");
 	}
-	
+
 	private void exp_list() throws IOException {
 		while(!cmatch(")")){
 			exp();
 		}
 	}
-	
+
 	private String[] filenames() {
 		String[] temp = new String[2];
 		temp[0] = file();
@@ -272,7 +359,7 @@ public class MiniREParser {
 		temp[1] = file();
 		return temp;
 	}
-	
+
 	private String file() {
 		String temp = null;
 		if(cmatch(Lexical.ASCIISTR)) {
@@ -281,7 +368,7 @@ public class MiniREParser {
 		token = itr.next();
 		return temp;
 	}
-	
+
 	private void find() throws IOException {
 		//find REGEX in <file>
 		String regex = null;
@@ -297,7 +384,7 @@ public class MiniREParser {
 			System.err.println("line "+token.getLinenum()+": invalid find call");
 		}
 	}
-	
+
 	private Object id() throws IOException {
 		String id_name = token.getTokenstr();
 		Object val = null;
@@ -312,7 +399,7 @@ public class MiniREParser {
 		}
 		return val;
 	}
-	
+
 	private boolean binop (int initial, String op) throws IOException {
 		Object b = 0;
 		if (nmatch(Lexical.INTNUM)) {
@@ -334,7 +421,7 @@ public class MiniREParser {
 			return false;
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void union_inters (Object initial, String op) throws IOException {
 		Object b = 0;
@@ -348,7 +435,7 @@ public class MiniREParser {
 		} else {
 			b = exp();
 		}
-		
+
 		if (b instanceof List) {
 			if(op.equals("union")) {
 				interp.union((List<Match>) initial,(List<Match>) b);
@@ -359,8 +446,8 @@ public class MiniREParser {
 			System.err.println("line "+token.getLinenum()+": mismatched types in binary operator");
 		}
 	}
-	
-	
+
+
 	private Object exp() throws IOException {
 		Object val= null;
 		if (match(Lexical.ID)){
@@ -377,7 +464,7 @@ public class MiniREParser {
 		} else if (match(";")) {
 			return null;
 		}
-		
+
 		if (nmatch("+")||nmatch("-")||nmatch("*")||nmatch("/")) {
 			token = itr.next();
 			if(val instanceof Integer) {
@@ -389,10 +476,10 @@ public class MiniREParser {
 				union_inters(val, token.getTokenstr());
 			}
 		}
-		
+
 		return val;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private Integer hash() throws IOException {
 		match("#");
@@ -403,12 +490,13 @@ public class MiniREParser {
 			return null;
 		}
 	}
-	
+
 	public void run(List<MiniREToken> toks) throws IOException {
 		token = toks.get(0);
-		itr = toks.iterator();
+		this.toks = toks;
+		itr = toks.listIterator();
 		begin();
 	}
-	
-	
+
+
 }
